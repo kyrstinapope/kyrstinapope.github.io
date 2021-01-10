1e8bb43ab1bde52b78ce55719b6763bc5a1643e6
kyrstinapope <kyrstina.pope@gmail.com>
2021-01-09 18:28:37 -0700

    blah

---
+++ date.js
@@ -0,0 +1,24 @@
+let daynames=[
+    "Sunday"
+    "Monday"
+    "Tuesday"
+    "Wednesday"
+    "Thursday"
+    "Friday"
+    "Saturday"
+    ]
+let months=[
+    "January"
+    "February"
+    "March"
+    "April"
+    "May"
+    "June"
+    "July"
+    "August"
+    "September"
+    "October"
+    "November"
+    "December"
+]
+let d=newdate();
