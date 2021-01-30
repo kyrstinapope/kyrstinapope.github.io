96dfa3bc8a81a1657974803fb6fa41b84e2c9694
kyrstinapope <kyrstina.pope@gmail.com>
2021-01-30 10:23:09 -0700

    done

--- lesson1/js/date.js
+++ lesson1/js/date.js
@@ -21,22 +21,24 @@ let months=[
     "November"
     "December"
 ]
+
 let d=newdate();
 let dayName = daynames[d.getDay()];
 let monthName = months[d.getMonth()];
-let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + d.getFullyear();
+let fulldate = dayName + ", " + d.getDate() + " " +  monthName + " " + d.getFullyear();
 document.getElementById("currentdate").textContent = fulldate;
-document.write(document.lastModified)
 
-try{
-    let options = {
-        weekday: "long";
-        day: "numeric";
-        month: "long";
-        year: "numeric";
 
-    }
-    document.getElementById(
-        "currentdate2").textContent = newDate().toLocaleDateString("en-US",optoins);
+// try{
+//     let options = {
+//         weekday: "long";
+//         day: "numeric";
+//         month: "long";
+//         year: "numeric";
 
-    }
+//     }
+//     document.getElementById(
+//         "currentdate2").textContent = newDate().toLocaleDateString("en-US",optoins);
+
+//     }
+
