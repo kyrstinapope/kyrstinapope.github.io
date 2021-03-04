7bcef07f202bfb386a2374285302ef6e054960a4
kyrstinapope <kyrstina.pope@gmail.com>
2021-03-03 19:34:21 -0700

    prophets

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -1,23 +1,38 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const requestURL =
+  "https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json";
 
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    for (let i = 0; i < prophets.length; i++ ) {
-});
+    console.table(jsonObject); // temporary checking for valid response and data parsing
+    const prophets = jsonObject["prophets"];
+    for (let i = 0; i < prophets.length; i++) {
+      let card = document.createElement("section");
+      let h2 = document.createElement("h2");
+      let p = document.createElement("p");
+      let p2 = document.createElement("p");
+      let img = document.createElement("img");
 
+      h2.textContent = prophets[i].name + " " + prophets[i].lastname;
+      p.textContent = "Date of Birth:" + " " + prophets[i].birthdate;
+      p2.textContent = "Place of Birth:" + " " + prophets[i].birthplace;
+      img.setAttribute("src", prophets[i].imageurl);
+      img.setAttribute(
+        "alt",
+        prophets[i].name +
+          " " +
+          prophets[i].lastname +
+          " — " +
+          prophets[i].order
+      );
 
-  const prophets = jsonObject['prophets'];
+      card.appendChild(h2);
+      card.appendChild(p);
+      card.appendChild(p2);
+      card.appendChild(img);
 
-  let card = document.createElement('section');
-let h2 = document.createElement('h2');
-
-h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-
-card.appendChild(h2);
-
-document.querySelector('div.cards').appendChild(card);
-image.setAttribute('src', prophets[i].imageurl);
+      document.querySelector("div.cards").appendChild(card);
+    }
+  });
