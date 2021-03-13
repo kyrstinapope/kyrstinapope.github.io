0577f2d5a171836a32098430206f2992744916cc
kyrstinapope <kyrstina.pope@gmail.com>
2021-03-12 18:12:13 -0700

    fix

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -15,10 +15,10 @@ fetch(apiURL)
     let prestonHigh = document.createElement("p");
     let prestonHumid = document.createElement("p");
     let prestonWind = document.createElement("p");
-    prestonTemp.textContent = `Current Temp: ${jsObject.main.temp}`;
-    prestonHigh.textContent = `High: ${jsObject.main.temp_max}`;
-    prestonHumid.textContent = `Humidity: ${jsObject.main.humidity}`;
-    prestonWind.textContent = `Wind Speed: ${jsObject.wind.speed}`;
+    prestonTemp.textContent = `Current Temp: ${jsObject.main.temp} \xB0F`;
+    prestonHigh.textContent = `High: ${jsObject.main.temp_max} \xB0F`;
+    prestonHumid.textContent = `Humidity: ${jsObject.main.humidity}%`;
+    prestonWind.textContent = `Wind Speed: ${jsObject.wind.speed} mph`;
 
     card.appendChild(prestonTemp);
     card.appendChild(prestonHigh);
