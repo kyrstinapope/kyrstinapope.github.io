1ac68b03c2c69eb55c44b633abdee28c0e43d71f
kyrstinapope <kyrstina.pope@gmail.com>
2021-03-15 19:19:26 -0600

    fix

--- lesson11/js/forecast.js
+++ lesson11/js/forecast.js
@@ -1,45 +1,65 @@
-let cityID= 5604473;
-let appID='9db90e96623f80837ffcf7bc7b3efd6d';
-const apiURL =
-  "https://api.openweathermap.org/data/2.5/forecast?id=${cityID}&units=imperial&appid=${appID}";
+// let cityID= 5604473;
+// let appID='9db90e96623f80837ffcf7bc7b3efd6d';
+const forecastApiURL =
+  "http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=9db90e96623f80837ffcf7bc7b3efd6d";
 
-fetch(apiURL)
+fetch(forecastApiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    let day =0;
-    const dayofweek = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'];
-    theFive.forEach( x=>){
-            }
-    let d=new Date(x.dt_txt);
-console.log(d)
+    let day = 0;
+    const dayofweek = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+    var i = 0;
 
+    jsObject.list.forEach((hourForecast) => {
+      var hours = new Date(hourForecast.dt_txt).getHours();
+      var day = new Date(hourForecast.dt_txt).getDay();
+      var temp = hourForecast.main.temp;
 
-    document.getElementById(`dayofweek${day+1}`).textContent = dayofweek[d.getDay()];
-    document.getElementById(`forecast${day+1}`).textContent = x.main.temp;
-  }};
+      console.log(i);
+      if (hours == 18) {
+        // console.log(day);
+        console.log(temp);
+        // console.log(dayofweek[day]);
 
-//     jsObject.list.forEach((hourForecast) => {
-      
-//       var hours = new Date(hourForecast.dt_txt).getHours();
-//       if (hours == 18) {
-       
-//         let forTable = document.createElement("section");
-//         let forecast-temp1 = document.createElement("td");
-//         let forecast-temp2 = document.createElement("td");
-//         let forecast-temp3 = document.createElement("td");
-//         let forecast-temp4 = document.createElement("td");
-//         let forecast-temp5 = document.createElement("td");
+        // console.log(`dayofweek${i+1}`);
 
-//         forecast-temp1.textContent = `<span id="imagesrc"></span> &nbsp; <img src="" alt="" id="icon">><br />${jsObject.list[0].main.temp} \xB0F`;
-//         prestonHigh.textContent = `High: ${jsObject.list[0].main.temp_max} \xB0F`;
-//         prestonHumid.textContent = `Humidity: ${jsObject.list[0].main.humidity} %`;
-//         prestonWind.textContent = `Wind Speed: ${jsObject.list[0].wind.speed} mph`;
-//         forTable.appendChild(prestonTemp);
-//         forTable.appendChild(prestonHigh);
-//         forTable.appendChild(prestonHumid);
-//         forTable.appendChild(prestonWind);
-//         document.querySelector("forTable").appendChild(forTable);
-//       }
-//     });
-//   });
+        document.getElementById(`dayofweek${i + 1}`).textContent =
+          dayofweek[day];
+        document.getElementById(`forecast${i + 1}`).textContent = temp;
+        // document.getElementById(`imagesrc${i+1}`).setAttribute= images;
+        // document.getElementById('icon').setAttribute('src', imagesrc)=jsObject.weather.icon;
+        // let imagesInfo =images(jsObject.weather.icon, jsObject.weather.description);
+
+        i++;
+      }
+    });
+  });
+
+//   document.getElementById(`dayofweek${day+1}`).textContent = dayofweek[d.getDay()];
+//   document.getElementById(`forecast${day+1}`).textContent = x.main.temp;
+
+// function images(icon,description){
+
+//   const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; // note the concatenation
+//   const desc = jsObject.weather.description; // note how we reference the weather array
+//   document.getElementById('imagesrc').textContent = imagesrc; // informational specification only
+//   document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
+//   document.getElementById('icon').setAttribute('alt', desc);
+// }
+
+const iconApiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=6dc037b176bf45f10d9b47e146e18667";
+fetch(iconApiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+
+        console.log(jsObject);
+        // let ptemp = Math.round((jsObject.main.temp - 273.15) * (9 / 5) + 32)
+        // document.getElementById('current-temp').textContent = ptemp;
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather.icon + '.png'; // note the concatenation
+        const desc = jsObject.weather.description; // note how we reference the weather array
+        document.getElementById('imagesrc').textContent = imagesrc; // informational specification only
+        document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
+        document.getElementById('icon').setAttribute('alt', desc);
+    });
+    
---
+++ lesson11/js/windchill.js
@@ -0,0 +1,10 @@
+function windChill(temp,windSpeed) {
+    
+    if (temp > 50 && windSpeed < 3) {
+        windChill = "N/A";
+    } else {
+        var chill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temp * Math.pow(windSpeed, 0.16));
+        chill = Math.round(chill * 1) / 1;
+    }
+    document.getElementById('windChill').innerHTML = chill;
+}
