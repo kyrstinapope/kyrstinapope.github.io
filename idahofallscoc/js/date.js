5ce1d7a12daf01935f4e3a7269598dcbd7fd38e8
kyrstinapope <kyrstina.pope@gmail.com>
2021-03-29 17:40:30 -0600

    start

---
+++ idahofallscoc/js/date.js
@@ -0,0 +1,9 @@
+const todaysDate = new Date();
+const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric"
+};
+document.getElementById("currentDate").textContent = todaysDate.toLocaleDateString('en-us', options);
+
