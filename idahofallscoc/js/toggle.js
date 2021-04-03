779f37c1bba2d40a4b7840bd75b728739cd39746
kyrstinapope <kyrstina.pope@gmail.com>
2021-04-02 22:32:39 -0600

    starting

--- idahofallscoc/js/toggle.js
+++ idahofallscoc/js/toggle.js
@@ -1,3 +1,23 @@
 function toggleMenu() {
-    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+  document
+    .getElementsByClassName("navigation")[0]
+    .classList.toggle("responsive");
 }
+
+
+  var elements = document.getElementsByClassName("column");
+
+  var i;
+
+  function listView() {
+    for (i = 0; i < elements.length; i++) {
+      elements[i].style.width = "100%";
+    }
+  }
+
+  function gridView() {
+    for (i = 0; i < elements.length; i++) {
+      elements[i].style.width = "50%";
+    }
+  }
+
